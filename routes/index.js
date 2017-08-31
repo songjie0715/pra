18dfabfcfb13a8192b399061d262afccc407b072
songjie_motie <songjie@motie.com>
2017-08-31 17:41:54 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var fs = require('fs');
+var path = require('path');
 var express = require('express');
 var router = express.Router();
 let multer = require('multer');
-var upload = multer({dest: 'upload/'});
+var upload = multer({dest: 'uploads/'});
 var app = express();
 
 /* GET home page. */
@@ -16,9 +17,25 @@ router.post('/createuser', upload.array('myForm'), function (req, res, next) {
 });
 
 router.post('/upload', upload.single('logo'), function(req, res, next){
-  console.log(req.file);
-  console.log('hahahah')
-  res.send({ret_code: '0'})
+
+    var filePath = req.file.path;
+    var originFileName = req.file.originalname;
+    console.log(req.file.originalname);
+
+    fs.readFile(filePath, function(err,data){
+        var timestamp = Date.now();
+        var type = req.file.mimetype.split('/')[1];
+        var poster = timestamp+'.'+type;
+
+        var newPath = path.dirname('/public/uploads/');
+        console.log(newPath);
+
+        fs.writeFile(newPath, function(err){
+            req.file.poster = poster;
+            res.send({imgSrc: req.file.poster});
+        })
+    })
+
 });
 
 module.exports = router;
